3beb7f86e02e22064ca35642f81051288c005ed3
Rinel <rineliniguezsosa@gmail.com>
2026-03-20 13:39:40 -0600

    [FEAT] MUltiples cambios en el archivo main.ts
    - eliminando el atributo personalizado data-id
    - creando la variable extensionInactive en donde filtramos las extensiones el cual tengan la prop isactive en false
    - creamos la varianle extensionsInactive para mapear justamente las extensiones inactivas
    - creamos el objeto views en donde se tipo la clave y valor
    - creé las claves all y inactive el cual devolvera dinamicamente el contenido de las extensiones
    - en extensList devolveremos inicialmente la vista ó el contenido de all
    - mediante  el target obtenemos el atributo personalizado de view
    - tambien pude obtener el atributo personalizado mediante getAttribute, sucede que no estaba estableciendo adecuadamente el atributo personalizado y por eso lo estuve obteniendo muktiples veces
    - en la linea 106 establecie el contenido dinamico para que se muestre correspondiente a la vista
    - movi claramenre el contenido de eliminar las extensiones al final ya que claramente primero mostramos la vista y después eliminamos las extensiones

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ toggleThemeButton.addEventListener('click', () => {
 
 const extensionsAll = data.map((extension) => {
   return `
-        <article data-id="${extension.id}" class="min-h-50 max-w-100 w-full flex flex-col 
+        <article class="min-h-50 max-w-100 w-full flex flex-col 
         items-stretch rounded-2xl p-5 bg-card desktop:max-w-[33%]">
           <div class="flex flex-row">
             <div class="w-1/4">
@@ -48,7 +48,66 @@ const extensionsAll = data.map((extension) => {
         </article>`;
 }).join('');
 
-extensionList.innerHTML = extensionsAll;
+let extensionInactive = extensions.filter((item) => item.isActive === false);
+
+const extensionsInactive = extensionInactive.map((extension) => {
+  return `
+        <article class="min-h-50 max-w-100 w-full flex flex-col 
+        items-stretch rounded-2xl p-5 bg-card desktop:max-w-[33%]">
+          <div class="flex flex-row">
+            <div class="w-1/4">
+              <img src="${extension.logo}" alt="${extension.name} Icon" />
+            </div>
+            <div class="w-3/4 px-1 pb-5">
+              <h2 class="text-xl font-bold font-notosans text-title">${extension.name}</h2>
+              <p class="text-base font-normal font-notosans text-text">${extension.description}</p>
+            </div>
+          </div>
+          <div class="flex items-center justify-between">
+            <button data-id="${extension.id}" class="rounded-full border-2 border-Neutral-300 px-4 py-1 font-notosans font-medium
+                  bg-buttonRemove text-text removeExtensionButton">
+              Remove
+            </button>
+
+            <label class="relative inline-block w-9.5 h-5 cursor-pointer">
+
+              <input type="checkbox" class="peer sr-only" ${extension.isActive ? 'checked' : ''} />
+              <div class="absolute inset-0 ${extension.isActive ? 'bg-Red-400' : 'bg-gray-300'} rounded-[34px] transition duration-400
+                  peer-checked:peer-focus:shadow-[0_0_1px_#2196F3]">
+              </div>
+              <div class="absolute left-1 bottom-1 w-3.5 h-3.5 bg-white rounded-full
+                  transition duration-400
+                  peer-checked:translate-x-4.5">
+              </div>
+            </label>
+          </div>
+        </article>`;
+}).join('');
+
+let views: { [key: string]: string } = {
+  all: extensionsAll,
+  inactive: extensionsInactive
+}
+
+extensionList.innerHTML = views.all;
+
+buttonState.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const target = event.currentTarget as HTMLButtonElement;
+    const view = button.dataset.view as string;
+
+    console.log(view);
+    console.log(button.getAttribute('data-view'));
+    console.log(target.getAttribute('data-view'));
+    console.log(target.dataset.view);
+    
+    
+    
+    extensionList.innerHTML = views[view];
+    buttonState.forEach((btn) => btn.classList.remove('active'));
+    target.classList.add('active');
+  })
+});
 
 const removeExtensionButton = document.querySelectorAll('.removeExtensionButton') as NodeListOf<HTMLButtonElement>;
 
@@ -70,11 +129,3 @@ removeExtensionButton.forEach((button) => {
 
   })
 })
-
-buttonState.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    const target = event.currentTarget as HTMLButtonElement; 
-    buttonState.forEach((btn) => btn.classList.remove('active'));
-    target.classList.add('active');
-  })
-});
